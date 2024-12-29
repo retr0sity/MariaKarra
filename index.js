6b49250a51337a7770a7529dc8931e6f5132ff26
Aggelos-Bog <aggelosvougiouklakhs@gmail.com>
2024-12-29 19:20:53 +0200

    necessary files added

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//yeetos
+// Import required packages
+import express from "express";
+
+// Initialize Express app and set port
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+  });
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});

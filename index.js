dddcd9561fded6accfe13d705c3837f2afd0b4e8
Aggelos-Bog <aggelosvougiouklakhs@gmail.com>
2024-12-29 21:44:55 +0200

    Created Homepage

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// Set up middleware
+app.use(express.static("public"));  
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
   });

f761bc599ed46a78c2f5eb9cb46f3aa4bc4abf7d
Aggelos-Bog <aggelosvougiouklakhs@gmail.com>
2025-01-07 20:06:33 +0200

    updated styles in work page

--- MariaKarra_new_version/js/work.js
+++ MariaKarra_new_version/js/work.js
@@ -5,8 +5,8 @@ projects.forEach(project => {
     div.className = "col-md-4 mb-4";
 
     div.innerHTML = ` <a href="./project.html?id=${project.id}">
-                        <img src=${project.images[0]} class="img-fluid img-shadow" alt=${project.title}>
-                        <h5 class="text-center mt-2">${project.title}</h5>
+                        <img src=${project.images[0]} class="img-fluid img-shadow project-image" alt=${project.title}>
+                        <h5 class="project-title">${project.title}</h5>
                     </a>`;
 
     document.querySelector(".row").appendChild(div);
